eb21e1280ac5a5dad2c9e4d73255e1436e95b20a
Elian Batista <elianbatista2000@gmail.com>
2023-10-27 14:58:12 -0300

    feat: add new permissions in widget embedded

--- src/embedded.js
+++ src/embedded.js
@@ -16,7 +16,7 @@ function Clicksign(key) {
     iframe = document.createElement('iframe');
     iframe.setAttribute('src', src);
     iframe.setAttribute('style', 'width: 100%; height: 100%;');
-    iframe.setAttribute('allow', 'camera;geolocation;fullscreen');
+    iframe.setAttribute('allow', 'camera;geolocation;fullscreen;gyroscope;accelerometer;magnetometer');
 
     window.addEventListener('message', handle);
 
